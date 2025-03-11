ebdecffdc7dc725ea147efcc8b9ad53ec4f6b6a7
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2025-03-10 21:37:43 -0300

    inhabilitación de estilos predeterminados

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './App.jsx'
+//import App from './App.jsx'
 import Rutas from './Routes/index.jsx'
 import { BrowserRouter } from 'react-router'
 
